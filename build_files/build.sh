ecfd7ede5a1fdcd4179d28ca099fe64a71ff5e24
wesley van tilburg <wesley.van.tilburg@referit.nl>
2026-02-14 17:00:24 +0100

    feat: add core-utils

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ouex pipefail
 
-dnf -y install sudo
-useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
+dnf -y install sudo shadow-utils
+sudo useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
 systemctl enable podman.socket
