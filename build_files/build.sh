b1543500050a3074a8e0597c1892584774af292c
wesley van tilburg <wesley.van.tilburg@referit.nl>
2026-02-14 16:03:55 +0100

    build: Add admin to wheel

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ouex pipefail
 
-sudo useradd -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
+sudo useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
 systemctl enable podman.socket
