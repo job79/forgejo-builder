0a4331282a57711fe9cdb344dccf4e989b73d01a
wesley van tilburg <wesley.van.tilburg@referit.nl>
2026-02-14 17:23:18 +0100

    mend

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,5 +2,6 @@
 set -ouex pipefail
 
 dnf -y install sudo shadow-utils
+mkdir -p /home/
 sudo useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
 systemctl enable podman.socket
