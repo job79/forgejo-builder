8b6573a2f2b5bca73aea210e2331001ceea71bab
Job79 <job@plabble.org>
2026-02-15 14:12:28 +0100

    feat: disable kernel args

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,16 +2,16 @@
 set -ouex pipefail
 
 # Fix home dir
-rm -rf /home
-ln -s /var/home /home
-mkdir -p /var/home
-chmod 755 /var/home
+# rm -rf /home
+# ln -s /var/home /home
+# mkdir -p /var/home
+# chmod 755 /var/home
 
 # Install packages
-dnf -y install sudo shadow-utils iptables-nft NetworkManager iproute hostname
+# dnf -y install sudo less shadow-utils iptables-nft NetworkManager iproute
 
 # Add user
-sudo useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
+useradd -g wheel -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
 
 # Disble kernel console messages
 echo "kargs = [\"loglevel=4\"]" >/usr/lib/bootc/kargs.d/loglevel.toml
