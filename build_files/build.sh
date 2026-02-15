3b927a6dbcde4e8ce7c86952efc42c019fcc757b
Job79 <job@plabble.org>
2026-02-15 13:39:50 +0100

    fix: disable selinux logging in console

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -ouex pipefail
 
+# Fix home dir
+rm -rf /home
+ln -s /var/home /home
+mkdir -p /var/home
+chmod 755 /var/home
+
+# Install packages
 dnf -y install sudo shadow-utils iptables-nft NetworkManager iproute hostname
+
+# Add user
 sudo useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' admin
-systemctl enable podman.socket
+
+# Disble kernel console messages
+echo "kargs = [\"loglevel=4\"]" >/usr/lib/bootc/kargs.d/loglevel.toml
