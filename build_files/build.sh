9b2c65614251e3857fd116e905c3692da67220fe
Job79 <job@plabble.org>
2026-03-01 15:30:59 +0100

    feat: add service file

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,13 @@ dnf -y install sudo rpm-ostree podman skopeo shadow-utils iptables-nft NetworkMa
 # Add user
 useradd -g wheel -m -p '$6$t8FzFHM1/JNawaqT$mUBJK8O/qIenmS4XaPrnSHjfEYchFj99jGRG5ectEe/2eYu9LLrWKLHZuAEfnWEE4JFIzUYX94.RTOgklPypS.' runner --base-dir /var/home
 
-systemctl enable podman.socket
-
 export RUNNER_VERSION=$(curl -X 'GET' https://data.forgejo.org/api/v1/repos/forgejo/runner/releases/latest | jq .name -r | cut -c 2-)
 export FORGEJO_URL="https://code.forgejo.org/forgejo/runner/releases/download/v${RUNNER_VERSION}/forgejo-runner-${RUNNER_VERSION}-linux-amd64"
 wget -O forgejo-runner ${FORGEJO_URL}
 chmod +x forgejo-runner
 mv forgejo-runner /usr/local/bin/forgejo-runner
+cp /ctx/forgejo-runner.service /etc/systemd/user/forgejo-runner.service
+
+# Enable forgejo-runner for the runner user
+su runner -c "systemctl --user enable podman.socket"
+su runner -c "systemctl --user enable forgejo-runner.service"
